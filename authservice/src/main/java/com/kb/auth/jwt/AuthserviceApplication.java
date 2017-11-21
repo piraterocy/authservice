a239e223342d1be6736615232bc1e05f12829901
piraterocy <zhang_xieyao@hotmail.com>
2017-11-21 15:48:47 +0800

    test

--- authservice/src/main/java/com/kb/auth/jwt/AuthserviceApplication.java
+++ authservice/src/main/java/com/kb/auth/jwt/AuthserviceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class AuthserviceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AuthserviceApplication.class, args);
 	}
 }
